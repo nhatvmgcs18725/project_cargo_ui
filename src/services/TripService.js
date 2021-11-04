b214e3232fc34d633f080e40a4cb1bcf1c1821d4
nhật vũ <nhatvmgcs18725@fpt.edu.vn>
2021-11-04 19:03:32 +0700

    make it betterr

--- src/services/TripService.js
+++ src/services/TripService.js
@@ -10,7 +10,7 @@ export let messages;
 export const connect = () => {
   if (!_socket || _socket.closed) {
     const token = getAccessToken();
-    _socket = webSocket(`wss://letsgoo-backend.herokuapp.com/taxi/?token=${token}`);
+    _socket = webSocket(`wss://letgo-admin-backend.herokuapp.com/taxi/?token=${token}`);
     messages = _socket.pipe(share());
     messages.subscribe(message => console.log(message));
   }
